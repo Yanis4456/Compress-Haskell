c1ee355b39ad8e89a772010433d95d4969624344
Yanis4456 <120134261+Yanis4456@users.noreply.github.com>
2024-03-23 20:09:55 +0100

    Update RLE.hs

--- src/RLE.hs
+++ src/RLE.hs
@@ -7,9 +7,17 @@ module RLE(compress, uncompress) where
 
 -- | RLE compress method
 compress :: Eq a => [a] -> [(a, Int)]
-compress _ = undefined -- TODO
+compress [] = []
+compress (x:xs) = compress' xs x 1
+  where
+    compress' :: Eq a => [a] -> a -> Int -> [(a, Int)]
+    compress' [] currentRun runLength = [(currentRun, runLength)]
+    compress' (y:ys) currentRun runLength
+      | y == currentRun = compress' ys currentRun (runLength + 1)
+      | otherwise       = (currentRun, runLength) : compress' ys y 1
 
 -- | RLE uncompress method
 -- If input cannot be uncompressed, returns `Nothing`
 uncompress :: [(a, Int)] -> Maybe [a]
-uncompress _ = undefined -- TODO
+uncompress [] = Just []
+uncompress xs = sequence $ concatMap (\(x, n) -> replicate n (Just x)) xs
