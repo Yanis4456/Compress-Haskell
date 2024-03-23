e2fb4e5e68c58b7aacb22cd01fae9885c7549fda
Yanis4456 <120134261+Yanis4456@users.noreply.github.com>
2024-03-23 21:18:32 +0100

    Update RLE.hs

--- src/RLE.hs
+++ src/RLE.hs
@@ -5,19 +5,18 @@
 -}
 module RLE(compress, uncompress) where
 
--- | RLE compress method
 compress :: Eq a => [a] -> [(a, Int)]
 compress [] = []
-compress (x:xs) = compress' xs x 1
-  where
-    compress' :: Eq a => [a] -> a -> Int -> [(a, Int)]
-    compress' [] currentRun runLength = [(currentRun, runLength)]
-    compress' (y:ys) currentRun runLength
-      | y == currentRun = compress' ys currentRun (runLength + 1)
-      | otherwise       = (currentRun, runLength) : compress' ys y 1
+compress (x:xs) = compressHelper xs x 1
+ where
+   compressHelper :: Eq a => [a] -> a -> Int -> [(a, Int)]
+   compressHelper [] currentRun runLength = [(currentRun, runLength)]
+   compressHelper (y:ys) currentRun runLength
+     | y == currentRun = compressHelper ys currentRun (runLength + 1)
+     | otherwise       = (currentRun, runLength) : compressHelper ys y 1
 
--- | RLE uncompress method
--- If input cannot be uncompressed, returns `Nothing`
+
+
 uncompress :: [(a, Int)] -> Maybe [a]
 uncompress [] = Just []
-uncompress xs = sequence $ concatMap (\(x, n) -> replicate n (Just x)) xs
+uncompress xs = Just $ concatMap (\(x, n) -> replicate n x) xs
